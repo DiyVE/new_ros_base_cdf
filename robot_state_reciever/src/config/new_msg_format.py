bebc23812c6637afd1b123253ee27fbb3f803177
DiyVE <ramirez.clement3@gmail.com>
2022-09-01 14:38:30 +0200

    init

--- robot_state_reciever/src/config/new_msg_format.py
+++ robot_state_reciever/src/config/new_msg_format.py
@@ -1,30 +1,30 @@
 from cdf_msgs.msg import RobotData
 
 msgs_dict = {
-    "test" : {
+    "motor" : {
         "robot_data": {
             "topic_rosparam": "~robot_odom_topic",
             "topic_default": "/robot_x/Odom",
             "type": RobotData,
             "publish_on_change": False,
             "structure": {
-                0: {
+                2: {
                     "value_expression": lambda x: x,
                     "path": "position.x"
                 },
-                1: {
+                3: {
                     "value_expression": lambda x: x,
                     "path": "position.y"
                 },
-                2: {
+                4: {
                     "value_expression": lambda x: x,
                     "path": "velocity.x"
                 },
-                3: {
+                5: {
                     "value_expression": lambda x: x,
                     "path": "velocity.y"
                 },
-                4: {
+                6: {
                     "value_expression": lambda x: x,
                     "path": "theta"
                 }
--- script_match/src/config/scripts/simulation.py
+++ script_match/src/config/scripts/simulation.py
@@ -5,76 +5,10 @@ from config.actions_2022 import *
 
 action_sequence = [
     {
-        "description": "on attend 5 secondes",
-        "function": tempo,
-        "params": {
-            "time": 5
-        }
-    },
-    {
-        "description": "on change la couleur en bleu",
-        "function": ledrgb,
-        "params": {
-            "r": 0,
-            "g": 0,
-            "b": 255
-        }
-    },
-    {
-        "description": "On va en x=2.5 et y=1",
-        "function": chainedmove,
-        "params": {
-            "x": 2.5,
-            "y": 1
-        }
-    },
-    {
-        "description": "On change la couleur en rouge",
-        "function": ledrgb,
-        "params": {
-            "r": 255,
-            "g": 0,
-            "b": 0
-        }
-    },
-    {
-        "description": "On va en x=1 et y=1.5",
-        "function": chainedmove,
-        "params": {
-            "x": 1,
-            "y": 1.5
-        }
-    },
-    {
-        "description": "On va en x=1 et y=1.5",
-        "function": chainedmove,
-        "params": {
-            "x": 2,
-            "y": 1.75
-        }
-    },
-    {
-        "description": "On change la couleur en vert",
-        "function": ledrgb,
-        "params": {
-            "r": 0,
-            "g": 255,
-            "b": 0
-        }
-    },
-    {
-        "description": "On rentre a la maison",
-        "function": chainedmove,
-        "params": {
-            "x": 0,
-            "y": 0
-        }
-    },
-    {
         "description": "tempo tres longue",
         "function": tempo,
         "params": {
-            "time": 100
+            "time": 1000
         }
     }
 ]
